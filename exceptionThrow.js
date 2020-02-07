adad64ccb9b236f57314da3298779c92db19c250
laxatzi <laxatzi@yahoo.com>
2020-02-07 16:53:50 +0200

    create valitation function using try/catch

--- exceptionThrow.js
+++ exceptionThrow.js
@@ -1,1 +1,21 @@
+function validateFn(){
+      var message, x;
+      message = document.getElementById('firstP');
+      message.innerHTML = "";
+      x= document.getElementById('demo').value;
+      try {
+         if(x == "") throw "is empty";
+         if(isNaN(x)) throw "is not a number";
+         x = Number(x);
+         if(x>10) throw "is too high!";
+         if(x<5) throw "is too low!";
+      }
+      catch(err){
+         message.innerHTML = "Error: "+ err +".";
+      }finally{
+         document.getElementById("demo").value = "";
+      }
+   }
+
+
 
